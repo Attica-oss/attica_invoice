cf311b603a654a624b992a67cd09954161ade15f
Garry Mounac <g.mounac@outlook.com>
2025-03-17 15:48:21 +0400

    Create sheet_ids.py

---
+++ data_source/sheet_ids.py
@@ -0,0 +1,54 @@
+"""Sheet IDs and Sheet Name"""
+
+# Miscellaneous Services
+MISC_SHEET_ID:str = "1VbfiiWsp8yxs6KSR1CXpw1S_35tYlWV8UjjWah9Afpw"
+
+CROSS_STUFFING_SHEET:str= "CrossStuffing"
+ALL_CCCS_DATA_SHEET:str="CCCSReport"
+CCCS_STUFFING_SHEET:str="CCCSContainerStuffing"
+BY_CATCH_SHEET:str = "IPHSBycatchTransfer"
+
+
+
+
+# Transport Services
+TRANSPORT_SHEET_ID:str = '1O8K26c7CqLSdLr-f2gvZliDpBn9ArxXvj9tEJy-ElUg'
+
+shore_crane_sheet="ShoreCrane"
+transfer_sheet="Transfer"
+forklift_sheet="ForkliftRecord"
+scow_sheet="ScowTransfer"
+
+# Stuffing Services
+
+STUFFING_SHEET_ID:str = "1L0HlevB-asshOgXMmIQ14bysq56lUPp0lYCrHPB0iGg"
+
+liner_pallet_sheet="LinerPallet"
+plugin_sheet="containerOperations"
+
+# EMR Services
+EMR_SHEET_ID:str = "1sPK_pSIJ8iR38yJExtvzGgJO2aMz2VN6_uDHh37TqR0"
+
+shifting_sheet = "ContainerShifting"
+washing_sheet = "ContainerCleaning"
+pti_sheet = "PTI"
+
+# Unloading Services
+OPS_SHEET_ID:str ="1PvTkl6DYZdhtaiNshz0qwtSPxC8S1OOeu905NmhFKNs"
+
+net_list_sheet = "UnloadingSummary"
+raw_sheet = "RawData"
+
+# Master Validation / Pricing
+MASTER_ID:str = "1ai-zQMtbPUx0LeQeLmXcpPgKvL5cyvwDfSJqRzxfUQg"
+price_sheet:str = "Price"
+client_sheet:str="Client"
+
+
+SHORE_HANDLING_ID:str="1R1M5La8lqF7NjJa7qspfPYvHU_Ej2o4YX7gduxb-Zu8"
+salt_sheet="SaltOperation"
+bin_tipping_sheet="BinTipping"
+
+INVOICING:str="1e1MZipsvEe5DxDaWaP2K2OBlCS_K0HEcPlFeyGzkG0s"
+invoice_status="InvoiceStatus"
+additional_stevedores="AdditionalStevedores"
